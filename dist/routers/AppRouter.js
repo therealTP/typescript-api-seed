8dae0538f4e420a936dea49e3654818e70ef755a
therealtp <mr.tylerpalmer@gmail.com>
2017-06-13 22:53:34 -0700

    removed dist

--- dist/routers/AppRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-class AppRouter {
-    constructor() {
-        this.router = express_1.Router();
-    }
-}
-exports.AppRouter = AppRouter;
